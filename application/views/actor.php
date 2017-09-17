603b34dcfc8648a925c9f074dd5bb50f1494f4a5
gyip16 <gyip16@my.bcit.ca>
2017-09-17 05:15:50 -0700

    actor page now shows role image and role quotes.

--- application/views/actor.php
+++ application/views/actor.php
@@ -3,5 +3,8 @@
                 <a href="/welcome/show/{key}">
                         <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
                 </a>
+				<h4>Quotes by {who}:</h4>
+				<img src="/pix/{role}.jpg" title="{role}"/>
+				<p><b>{role}: </b>{what}</p>
         </div>
 </div>
